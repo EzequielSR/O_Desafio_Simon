bcfedc4d1d300d0eea579bb87eeef62addb7d815
Ezequiel de Souza Rodrigues <ezequieldesr@gmail.com>
2024-07-08 18:05:26 -0300

    add functions animatePress and playSound

--- game.js
+++ game.js
@@ -6,19 +6,32 @@ $(".btn").click(function() {
     var userChosenColour =  $(this).attr("id")
     userClickedPattern.push(userChosenColour)
     
+    playSound(userChosenColour)
+    animatePress(userChosenColour)
 });
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed")
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed")
+    },100)
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3")
+    audio.play();
+    
+}
+
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4)
     var randomChosenColour = buttonColours[randomNumber]
     gamePattern.push(randomChosenColour)
-
+    
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
-
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3")
-    audio.play();
-
+    
+    playSound(randomChosenColour)
 }
-nextSequence()
+
 
 
