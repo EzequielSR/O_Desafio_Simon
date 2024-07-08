6dad0e99474aae6d38df5d1a54559031212a872e
Ezequiel de Souza Rodrigues <ezequieldesr@gmail.com>
2024-07-08 17:39:38 -0300

    update js

--- game.js
+++ game.js
@@ -1,5 +1,12 @@
 var buttonColours = ["red", "blue", "green", "yellow"]
 var gamePattern = []
+var userClickedPattern = []
+
+$(".btn").click(function() { 
+    var userChosenColour =  $(this).attr("id")
+    userClickedPattern.push(userChosenColour)
+    
+});
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4)
@@ -10,7 +17,8 @@ function nextSequence() {
 
     var audio = new Audio("sounds/" + randomChosenColour + ".mp3")
     audio.play();
-}
 
+}
+nextSequence()
 
 
