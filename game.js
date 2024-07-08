bc833b62629c35681e0ea2242294892561187bc3
Ezequiel de Souza Rodrigues <ezequieldesr@gmail.com>
2024-07-08 16:46:39 -0300

    Add audio and animation on buttons

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
-var gamePattern =[]
-var buttonColours = ["red","blue","green","yellow"]
+var buttonColours = ["red", "blue", "green", "yellow"]
+var gamePattern = []
 
-function nextSequence(){
+function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4)
-    // return buttonColours[randomNumber]
     var randomChosenColour = buttonColours[randomNumber]
-    gamePattern.push(randomChosenColour) 
-    // var randomChosenColour = randomNumber
+    gamePattern.push(randomChosenColour)
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3")
+    audio.play();
 }
-nextSequence()
-console.log(gamePattern)
+
+
+
